db0ef978f49e3fe780f276813addd03b6dfb3cf8
Eury Rivera <euryrivera@hotmail.com>
2018-08-20 14:44:51 -0700

    Start code

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+var cacheVersion = 'V1';
+var cacheFiles = [
+    './',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/dbhelper.js',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './index.html',
+    './restaurant.html'
+]
+
+self.addEventListener('install', function(e){
+    console.log("Installed")
+
+    e.waitUntil(
+        caches.open(cacheVersion).then(function(cache){
+            console.log('Caching files');
+            return cache.addAll(cacheFiles);
+        })
+    )
+})
+
+self.addEventListener('activate', function(e){
+    console.log("activated")
+
+    e.waitUntil(
+        caches.keys().then(function(cacheVersions){
+            return Promise.all(cacheVersions.map(function(thisCacheVersion){
+                if (thisCacheVersion !== cacheVersion) {
+                    console.log('Removing cache files from ', thisCacheVersion);
+                    return caches.delete(thisCacheVersion);
+                }
+            }))
+        })
+    )
+})
+
+self.addEventListener('fetch', function(e){
+    console.log("Fetching", e.request.url)
+})
